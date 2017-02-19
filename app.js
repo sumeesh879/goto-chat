01f83b824eeee47d09d88ba472cf804e7d1a3bab
sumeesh <sumeesh.nagisetty@gmail.com>
2017-02-19 15:23:09 -0500

    port update

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.get('*', (req, res) => {
 });
 
 //Start Server
-app.listen(port, () => {
-    console.log("Server Starting on port " + port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
